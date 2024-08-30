4a4a00091120121c04bf9d1047a211c9ac312c2d
GYLIMA <117772403+gyovannalima@users.noreply.github.com>
2024-08-30 17:05:07 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,143 @@
+export const perguntas = [
+    {
+        enunciado: "Qual você considera mais importante para o progresso na academia?",
+        alternativas: [
+            {
+                texto: "Aprender técnicas de treino corretas.",
+                afirmacao: [
+                    "Você valoriza o domínio das técnicas de treino como essencial para o progresso na academia.",
+                    "Acredita que executar os exercícios corretamente é fundamental para obter resultados eficazes."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Desenvolver habilidades de planejamento e disciplina.",
+                afirmacao: [
+                    "Você acredita que o planejamento eficaz e a disciplina são cruciais para o sucesso na academia.",
+                    "Vê o desenvolvimento de uma rotina estruturada e a autoconstância como a base para um bom desempenho físico."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Se você valoriza o aprendizado das técnicas de treino, qual destas opções você acha mais útil?",
+        alternativas: [
+            {
+                texto: "Sessões com personal trainer.",
+                afirmacao: [
+                    "Você acredita que trabalhar com um personal trainer proporciona instruções detalhadas e correções de técnica.",
+                    "Vê a orientação profissional como uma forma eficaz de aprender e aplicar as técnicas corretamente."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Assistir a vídeos e tutoriais online.",
+                afirmacao: [
+                    "Você valoriza o uso de vídeos e tutoriais como uma maneira prática de aprender novas técnicas de treino.",
+                    "Acredita que esses recursos oferecem uma forma acessível e flexível de adquirir conhecimento técnico."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Se você acha que vídeos e tutoriais online são mais úteis, como você vê a importância do acompanhamento presencial?",
+        alternativas: [
+            {
+                texto: "O acompanhamento presencial fornece feedback imediato e correção de postura.",
+                afirmacao: [
+                    "Você vê o acompanhamento presencial como uma forma importante de receber feedback direto e corrigir possíveis erros.",
+                    "Acredita que a orientação ao vivo é fundamental para garantir a execução correta dos exercícios."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "O acompanhamento presencial é menos relevante comparado à autonomia do treino online.",
+                afirmacao: [
+                    "Você considera que a autonomia e o aprendizado autodidata têm mais impacto do que o acompanhamento presencial.",
+                    "Vê o treino online como mais flexível e adaptável às suas necessidades pessoais."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Se você acredita que o acompanhamento presencial é importante, como você avalia a importância de manter-se atualizado com novas técnicas e tendências de treino?",
+        alternativas: [
+            {
+                texto: "Manter-se atualizado é crucial para adaptar os treinos e evitar estagnação.",
+                afirmacao: [
+                    "Você vê a atualização constante como uma necessidade para evitar a estagnação e adaptar os treinos às novas tendências.",
+                    "Acredita que estar informado sobre novas técnicas é vital para continuar evoluindo no treino."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Manter-se atualizado é secundário em comparação com a consistência no treino.",
+                afirmacao: [
+                    "Você considera que a consistência no treino é mais importante do que estar atualizado com as últimas tendências.",
+                    "Acredita que a regularidade e o foco são mais decisivos para alcançar resultados do que as novas técnicas."
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Se você considera importante manter-se atualizado, qual formato você acha mais eficaz para aprender novas técnicas de treino?",
+        alternativas: [
+            {
+                texto: "Participar de workshops e eventos de fitness.",
+                afirmacao: [
+                    "Você vê workshops e eventos como métodos eficazes para aprender diretamente com especialistas e experimentar novas técnicas.",
+                    "Acredita que esses eventos oferecem oportunidades práticas e de networking no mundo fitness."
+                ],
+            },
+            {
+                texto: "Leitura de artigos e blogs especializados.",
+                afirmacao: [
+                    "Você valoriza a leitura de artigos e blogs como uma forma de se atualizar e aprofundar seu conhecimento de maneira autônoma.",
+                    "Acredita que esses recursos proporcionam uma visão detalhada e bem pesquisada sobre técnicas e tendências."
+                ],
+            },
+        ]
+    },
+    {
+        enunciado: "Se você acha que a leitura de artigos é eficaz, como você vê a relação entre aprendizado autodidata e certificações de fitness?",
+        alternativas: [
+            {
+                texto: "O aprendizado autodidata pode ser tão eficaz quanto certificações de fitness, dependendo do contexto.",
+                afirmacao: [
+                    "Você considera que o aprendizado autodidata pode ser igualmente válido e útil, dependendo das circunstâncias e do contexto.",
+                    "Vê o autoestudo como uma abordagem flexível que pode complementar ou substituir certificações formais."
+                ],
+            },
+            {
+                texto: "Certificações de fitness são mais valiosas para garantir a qualidade e a validação do conhecimento adquirido.",
+                afirmacao: [
+                    "Você acredita que certificações formais oferecem um reconhecimento oficial e garantem a qualidade do conhecimento adquirido.",
+                    "Vê as certificações como uma forma importante de validar suas competências e habilidades no campo do fitness."
+                ],
+            },
+        ]
+    },
+    {
+        enunciado: "Se você acredita que certificações de fitness são mais valiosas, como você avalia a importância das habilidades interpessoais na prática de treino em grupo?",
+        alternativas: [
+            {
+                texto: "As habilidades interpessoais são essenciais para o sucesso e a coesão em treinos em grupo.",
+                afirmacao: [
+                    "Você considera que habilidades interpessoais são fundamentais para a eficácia e a dinâmica em treinos em grupo.",
+                    "Acredita que a capacidade de se comunicar e motivar os outros é tão importante quanto as qualificações técnicas."
+                ],
+            },
+            {
+                texto: "As habilidades interpessoais são importantes, mas as certificações e qualificações técnicas são mais decisivas para o sucesso no treino.",
+                afirmacao: [
+                    "Você vê as certificações e qualificações técnicas como os principais fatores para o sucesso e a progressão na prática de treino.",
+                    "Acredita que, embora as habilidades interpessoais sejam valiosas, elas não superam a importância das certificações formais."
+                ],
+            },
+        ]
+    }
+];
